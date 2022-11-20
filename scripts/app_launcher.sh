64f3bb81e869d094da4e8c01b937c04ebc793a38
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-11-20 16:57:06 +0100

    scripts/app_launcher.sh                        |  5 ---  scripts/ffmpeg_convert_mkv_to_mp4_with_subs.sh | 31 --------------  scripts/open_hyper_new_window.sh               | 10 -----  scripts/popup.sh                               | 17 --------  scripts/show_plists.sh                         | 56 --------------------------

--- scripts/app_launcher.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-find /System/Library/CoreServices /System/Applications /Applications /System/Applications/Utilities ~/Applications/Chrome\ Canary\ Apps.localized -maxdepth 1 -name "*.app" | fzf | xargs -I {} open "{}"
-kill -9 $PPID                        # this will kill the old terminal
-exit                                 # exit script

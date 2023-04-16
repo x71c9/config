b32e13a660f61c89985ab6c0fb21c869dbfdbf30
x71c9 <108585118+x71c9@users.noreplay.github.com>
2023-04-16 16:05:12 +0200

    changes from i7xps-arch on Sun 16 Apr 2023 16:05:12 CEST

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -7,9 +7,7 @@
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/usr/share/fzf/completion.bash" 2> /dev/null
-# [[ $- == *i* ]] && source "/opt/homebrew/Cellar/fzf/0.30.0/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
 source "/usr/share/fzf/key-bindings.bash"
-# source "/opt/homebrew/Cellar/fzf/0.30.0/shell/key-bindings.bash

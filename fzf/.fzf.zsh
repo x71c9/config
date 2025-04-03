1a1bb8395a012c2fffba21958d92f5baaaf0dcfe
x71c9 <108585118+x71c9@users.noreply.github.com>
2025-04-03 18:23:38 +0200

    changes from x71c9-m2mini.local on Thu Apr 3 18:23:38 CEST 2025

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -7,9 +7,15 @@
 # Auto-completion
 # ---------------
 # [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-[[ $- == *i* ]] && source "/opt/homebrew/Cellar/fzf/0.38.0/shell/completion.zsh" 2> /dev/null
+# [[ $- == *i* ]] && source "/opt/homebrew/Cellar/fzf/0.38.0/shell/completion.zsh" 2> /dev/null
+if [[ $- == *i* ]]; then
+  fzf_path="$(brew --prefix fzf)/shell/completion.zsh"
+  [[ -f "$fzf_path" ]] && source "$fzf_path"
+fi
 
 # Key bindings
 # ------------
 # source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-source "/opt/homebrew/Cellar/fzf/0.50.0/shell/key-bindings.zsh"
+# source "/opt/homebrew/Cellar/fzf/0.50.0/shell/key-bindings.zsh"
+key_bindings_path="$(brew --prefix fzf)/shell/key-bindings.zsh"
+[[ -f "$key_bindings_path" ]] && source "$key_bindings_path"
